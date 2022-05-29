f4e0df31ec35d4390baba49a12f2ad183ded27c0
diegolorenzo12 <72369829+diegolorenzo12@users.noreply.github.com>
2022-05-29 00:10:21 -0700

    Update Owner.sol
    agregar archivo owner

--- default_workspace/contracts/Owner.sol
+++ default_workspace/contracts/Owner.sol
@@ -1,1 +1,55 @@
+pragma solidity ^0.5.0;
 
+
+interface OwnersInterface{
+    function ownerExists(address _owner) external view returns(bool);
+    function getOwnerRole(address _owner) external view returns(uint);
+}
+
+
+contract Owners {
+
+    enum OwnerRole { 
+        Manufacter,    // 0
+        Distributor,   // 1
+        Retail,        // 2
+        Consumer       // 3
+    }
+
+    OwnerRole constant defaultState = OwnerRole.Manufacter;
+    
+    struct Owner{
+        string name;
+        string description;
+        OwnerRole role;
+        bool exist; //para revisar si existe, porque el mappping siempre arroja 0 y no null
+    }
+
+    mapping (address => Owner) owners;
+
+    function addOwner (string memory _name, string memory _description, OwnerRole _role) public{
+        owners[msg.sender] = Owner(_name, _description, _role, true);
+    }
+
+    function getOwner(address owners_addr) public view returns (string memory, string memory, OwnerRole){
+        return (owners[owners_addr].name, owners[owners_addr].description, owners[owners_addr].role);
+    }
+
+    function ownerExists (address _owner) external view returns (bool){
+        if(owners[_owner].exist == true ) {
+            return true;
+        }
+        return false;
+    }
+    function getOwnerRole(address _owner) external view returns(uint){
+        OwnerRole role = owners[_owner].role;
+        if(role == OwnerRole.Manufacter){
+            return 0;
+        }else if(role == OwnerRole.Distributor){
+            return 1;
+        }else if(role == OwnerRole.Retail){
+            return 2;
+        }
+        return 3;
+    }
+}
